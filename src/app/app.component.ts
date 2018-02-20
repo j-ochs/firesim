89d7596ceae615a46960b53cf6ff3033ede8a7fb
Jacob <jochs@westmont.edu>
2018-02-20 00:35:17 -0800

    custom control label, other work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { latLng, tileLayer, icon, Map, point, polyline, marker } from "leaflet";
+import { Http, Response, RequestOptions, Headers } from "@angular/http";
+import  * as L  from "leaflet";
+import 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,75 +12,68 @@ export class AppComponent {
   title = 'app';
 
 
+  constructor (private http: Http){
+
+  }
+
+
 
   // Define our base layers so we can reference them multiple times
-  googleMaps = tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+  googleMaps = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
     detectRetina: true
   });
-  googleHybrid = tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+  googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
     detectRetina: true
   });
-  openTopoMap = tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
-  Stamen_Toner = tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+  Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: 'abcd',
     minZoom: 0,
     maxZoom: 20,
     ext: 'png'
   });
-  openWeatherMap_Wind = tileLayer('http://{s}.tile.openweathermap.org/map/wind/{z}/{x}/{y}.png?appid={apiKey}', {
+  openWeatherMap_Wind = L.tileLayer('http://{s}.tile.openweathermap.org/map/wind/{z}/{x}/{y}.png?appid={apiKey}', {
     maxZoom: 19,
     attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
     apiKey: '0b9ae90c37b492b7da3c843ff795f217',
     opacity: 0.5
   });
-  openWeatherMap_Clouds = tileLayer('http://{s}.tile.openweathermap.org/map/clouds/{z}/{x}/{y}.png?appid={apiKey}', {
+  openWeatherMap_Clouds = L.tileLayer('http://{s}.tile.openweathermap.org/map/clouds/{z}/{x}/{y}.png?appid={apiKey}', {
     maxZoom: 19,
     attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
     apiKey: '0b9ae90c37b492b7da3c843ff795f217',
     opacity: 0.7
   });
-  openWeatherMap_Precipitation = tileLayer('http://{s}.tile.openweathermap.org/map/precipitation/{z}/{x}/{y}.png?appid={apiKey}', {
+  openWeatherMap_Precipitation = L.tileLayer('http://{s}.tile.openweathermap.org/map/precipitation/{z}/{x}/{y}.png?appid={apiKey}', {
     maxZoom: 19,
     attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
     apiKey: '0b9ae90c37b492b7da3c843ff795f217',
-    opacity: 0.7
+    opacity: 0.5
+  });
+  openWeatherMap_Temperature = L.tileLayer('http://{s}.tile.openweathermap.org/map/temp/{z}/{x}/{y}.png?appid={apiKey}', {
+    maxZoom: 19,
+    attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
+    apiKey: '0b9ae90c37b492b7da3c843ff795f217',
+    opacity: 0.4
   });
 
-  // Marker for the top of Mt. Ranier
-  summit = marker([ 46.8523, -121.7603 ], {
-    icon: icon({
+  // Marker for Westmont College
+  westmont = L.marker([ 34.4488, -119.6610 ], {
+    icon: L.icon({
       iconSize: [ 25, 41 ],
       iconAnchor: [ 13, 41 ],
       iconUrl: 'leaflet/marker-icon.png',
       shadowUrl: 'leaflet/marker-shadow.png'
     })
   });
-
-
-  // Path from paradise to summit - most points omitted from this example for brevity
-  route = polyline([[ 46.78465227596462,-121.74141269177198 ],
-    [ 46.80047278292477, -121.73470708541572 ],
-    [ 46.815471360459924, -121.72521826811135 ],
-    [ 46.8360239546746, -121.7323131300509 ],
-    [ 46.844306448474526, -121.73327445052564 ],
-    [ 46.84979408048093, -121.74325201660395 ],
-    [ 46.853193528950214, -121.74823296256363 ],
-    [ 46.85322881676257, -121.74843915738165 ],
-    [ 46.85119913890958, -121.7519719619304 ],
-    [ 46.85103829018772, -121.7542376741767 ],
-    [ 46.85101557523012, -121.75431755371392 ],
-    [ 46.85140013694763, -121.75727385096252 ],
-    [ 46.8525277543813, -121.75995212048292 ],
-    [ 46.85290292836726, -121.76049157977104 ],
-    [ 46.8528160918504, -121.76042997278273 ]]);
 
   // Layers control object with our two base layers and the three overlay layers
   layersControl = {
@@ -89,36 +84,85 @@ export class AppComponent {
       'B&W Contrast Map': this.Stamen_Toner
     },
     overlays: {
-      'Mt. Rainier Summit': this.summit,
-      'Mt. Rainier Climb Route': this.route,
+      'Westmont College': this.westmont,
       'Wind': this.openWeatherMap_Wind,
       'Clouds': this.openWeatherMap_Clouds,
-      'Precipitation': this.openWeatherMap_Precipitation
+      'Precipitation': this.openWeatherMap_Precipitation,
+      'Temperature': this.openWeatherMap_Temperature
     }
   };
 
+
+
+  
+
+  //map initialization variables
   options = {
     layers: [
-      tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
         maxZoom: 20,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
         detectRetina: true
       }),
-      
-      this.googleMaps, this.route, this.summit
+      this.googleMaps, this.westmont
     ],
     zoom: 7,
-    center: latLng([ 46.879966, -121.726909 ])
+    center: L.latLng([ 34.4488, -119.6610 ]),
   };
 
+  getData(coords: any) {
+    console.log("get data: ", coords, typeof(coords))
+    return this.http.get('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}')
+        .map(response => response = response.json());
+}
 
 
-  onMapReady(map: Map) {
-    map.fitBounds(this.route.getBounds(), {
-      padding: point(24, 24),
-      maxZoom: 12,
-      animate: true
-    });
+
+  onMapReady(map: L.Map) {
+    L.control.scale({metric:false}).addTo(map);
+
+    // var info = L.control({position: 'bottomleft'});
+    
+    // info.onAdd = function (map) {
+    //     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    //     this.update();
+    //     return this._div;
+    // };
+    
+    // // method that we will use to update the control based on feature properties passed
+    // info.update = function (props) {
+    //     this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+    //         '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+    //         : 'Hover over a state');
+    // };
+    
+    // info.addTo(map);
+
+
+
+
+
+
+
+    // map.on('click', function(e: any) {
+    //   console.log('mapclick: ', e.latlng, typeof(e.latlng)); 
+    //   return this.http.get('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}')
+    //   .map(response => response = response.json());
+
+
+
+     // this.getData(e.latLng);
+      
+      // var popLocation= e.;
+      // var popup = L.popup()
+      // .setLatLng(popLocation)
+      // .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+      // .openOn(map);        
+ // });
+    // map.fitBounds([
+    //   [40.712, -74.227],
+    //   [40.774, -74.125]
+    // ]);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from "@angular/http";
 import { LeafletModule } from "@asymmetrik/ngx-leaflet";
 import { AppComponent } from './app.component';
 
@@ -9,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    LeafletModule.forRoot()
+    LeafletModule.forRoot(),
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
